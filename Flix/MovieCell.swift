c083bcd7b2c88760d984814498001d045c441656
Audrey Jones <audreyjones@fb.com>
2017-06-21 16:18:35 -0700

    user can now view a table view of movies with the movie titles, poster images, and descriptions. README file is updated

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Audrey Jones on 6/21/17.
+//  Copyright © 2017 Audrey Jones. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

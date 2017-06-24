1b766b7541d75839275a37bc2e40fba7ef97515e
Audrey Jones <audreyjones@fb.com>
2017-06-23 17:00:16 -0700

    displays an error message when there's a networking error, added collection view that connects with the detail screen, and added a search bar

--- Flix/CollectionViewController.swift
+++ Flix/CollectionViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     var movies: [[String: Any]] = []
+    var filteredMovies : [[String: Any]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         collectionView.dataSource = self
         collectionView.delegate = self
+        searchBar.delegate = self
+        
+        activityIndicator.startAnimating()
+        
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        collectionView.insertSubview(refreshControl, at: 0)
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=3e7d90028adc30c483b7983f82af2a8b")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -31,7 +43,9 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
+                self.filteredMovies = movies
                 self.collectionView.reloadData()
+                self.activityIndicator.stopAnimating()
             }
             
         }
@@ -48,12 +62,12 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return movies.count
+        return filteredMovies.count
     }
     
    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionMovieCell", for: indexPath) as! CollectionMovieCell
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let baseURLString = "https://image.tmdb.org/t/p/w500"
         let posterPathString = movie["poster_path"] as! String
         let fullPosterPath = baseURLString + posterPathString
@@ -61,18 +75,70 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         cell.posterImage.af_setImage(withURL: posterURL!)
         return cell
     }
+    
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        // ... Create the URLRequest `myRequest` ...
+        
+        // Configure session so that completion handler is executed on main UI thread
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=3e7d90028adc30c483b7983f82af2a8b")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            }else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+                
+                refreshControl.endRefreshing()
+                
+            }
+            
+        }
+        task.resume()
+    }
  
  
- 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        
+        if searchText.isEmpty {
+            filteredMovies = movies
+        }
+        else {
+            filteredMovies = movies.filter { (movie: [String : Any]) -> Bool in
+                let title = movie["title"] as! String
+                return title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            }
+        }
+        
+        collectionView.reloadData()
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UICollectionViewCell
+        if let indexPath = collectionView.indexPath(for: cell) {
+        let movie = filteredMovies[indexPath.row]
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
+     
+     }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import AlamofireImage
 
-class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var movies: [[String: Any]] = []
+    
+    var filteredMovies: [[String : Any]] = []
+    
 
     override func viewDidLoad() {
         
@@ -24,16 +29,16 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         
         super.viewDidLoad()
         
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBar.delegate = self
+        
         // Initialize a UIRefreshControl
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
-        
-        
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        
+    
 
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=3e7d90028adc30c483b7983f82af2a8b")!
@@ -42,11 +47,19 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         let task = session.dataTask(with: request) { (data, response, error) in
             // This will run when the network request returns
             if let error = error {
-                print(error.localizedDescription)
+                let alertController = UIAlertController(title: "Alert", message: "Error loading", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    print(error.localizedDescription)
+                }
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true)
+                
             }else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
+                self.filteredMovies = movies
                 self.tableView.reloadData()
                 self.activityIndicator.stopAnimating()
                 
@@ -88,12 +101,12 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return filteredMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let baseURLString = "https://image.tmdb.org/t/p/w500"
@@ -110,16 +123,34 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as! UITableViewCell
         if let indexPath = tableView.indexPath(for: cell) {
-            let movie = movies[indexPath.row]
+            let movie = filteredMovies[indexPath.row]
             let detailViewController = segue.destination as! DetailViewController
             detailViewController.movie = movie
         }
-  
-        
         
     }
     
-    // not working
+    // This method updates filteredData based on the text in the Search Box
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+
+        if searchText.isEmpty {
+            filteredMovies = movies
+        }
+        else {
+            filteredMovies = movies.filter { (movie: [String : Any]) -> Bool in
+                let title = movie["title"] as! String
+                return title.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
